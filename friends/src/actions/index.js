442721b330470cae27cd8abf401a5ce0ad0345eb
oashtari <oashtari@gmail.com>
2020-02-16 18:53:35 -0800

    added ability to add a friend

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -5,6 +5,8 @@ export const LOAD_FRIENDS = 'LOAD_FRIENDS';
 export const ERROR_FRIENDS = 'ERROR_FRIENDS';
 
 export const ADD_FRIEND = 'ADD_FRIEND';
+export const FRIEND_ADDED = 'FRIEND_ADDED';
+export const ADD_ERROR = 'ADD_ERROR';
 
 export const getFriends = () => dispatch => {
     dispatch({ type: GET_FRIENDS });
@@ -25,13 +27,37 @@ export const getFriends = () => dispatch => {
 
 // ADD FRIEND NEEDED
 
-export const addFriend = () => dispatch => {
+export const addFriend = friend => dispatch => {
     dispatch({ type: ADD_FRIEND });
 
-    // setTimeout(() => {
-    //     axiosWithAuth()
-    //         .get(`/friends`)
-    //         .then(res => console.log(res))
-    //         .catch(err => console.log(err))
-    // }, 1000)
+    setTimeout(() => {
+        axiosWithAuth()
+            .post(`/friends`, friend)
+            .then(res => {
+                console.log(res);
+                dispatch({ type: FRIEND_ADDED, payload: res.data })
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch({ type: ADD_ERROR, payload: "could not add your friend" })
+            })
+    }, 1000)
 }
+
+
+// export const addSmurf = friend => dispatch => {
+//     dispatch({ type: ADD_SMURF });
+
+//     setTimeout(() => {
+//         axios
+//             .post(`/friends`, friend)
+//             .then(res => {
+//                 console.log(res);
+//                 dispatch({ type: SMURF_ADDED_SUCCESS, payload: res.data })
+//             })
+//             .catch(err => {
+//                 console.log(err);
+//                 dispatch({ type: SMURF_ADD_ERROR, payload: "couldn't add your smurf" })
+//             })
+//     }, 1000)
+// }
